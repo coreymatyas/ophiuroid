e06d90ff67f5e8c33b96839f635887788926cde9
cmhull42 <cmhull42@gmail.com>
2014-08-07 13:56:11 -0500

    Add gfycat ripper

---
+++ rippers/gfycat.js
@@ -0,0 +1,31 @@
+var common    = require("../common"),
+	http      = require("http"),
+	config    = require("../config").rip.gfycat,
+	request   = require("request"),
+	logger    = require("../log");
+
+var regex = /^https?:\/\/(?:www\.)?gfycat\.com\/(.+)/;
+
+	module.exports.name = "Gfycat";
+	module.exports.url = regex;
+	module.exports.authority = 1;
+
+	module.exports.rip = function(site) {
+		var groups = regex.exec(site.url);
+
+		request("http://gfycat.com/cajax/get/"+groups[1], function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var formats = config.formats;
+				var response = JSON.parse(body);
+				for (var format in formats) {
+					if (formats[format] == true) {
+						common.downloadFile(site, response["gfyItem"][format+"Url"]);
+					}
+				}
+				return true;
+			}
+			else {
+				return false;
+			}
+		});
+	}
